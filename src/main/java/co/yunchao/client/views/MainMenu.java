bfd66ee6d7dcca0094606c4fb1267b3e4b4bc0fd
tigerza117 <noppawanza1171@gmail.com>
2021-12-06 01:39:23 +0700

    ✨ Main menu view.

--- src/main/java/co/yunchao/client/views/MainMenu.java
+++ src/main/java/co/yunchao/client/views/MainMenu.java
@@ -4,28 +4,24 @@ import com.almasb.fxgl.animation.Animation;
 import com.almasb.fxgl.animation.Interpolators;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
-import javafx.beans.binding.StringBinding;
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.CacheHint;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
-import static javafx.beans.binding.Bindings.createStringBinding;
-import static javafx.beans.binding.Bindings.when;
 
 public class MainMenu extends FXGLMenu {
-    private List<Node> buttons = new ArrayList<>();
-    private List<Animation<?>> animations = new ArrayList<>();
+    private final List<Node> buttons = new ArrayList<>();
+    private final List<Animation<?>> animations = new ArrayList<>();
 
     private int animIndex = 0;
 
@@ -33,32 +29,23 @@ public class MainMenu extends FXGLMenu {
         super(MenuType.GAME_MENU);
 
         var bg = texture("background.png", getAppWidth(), getAppHeight());
-
-        var homeLogo = texture("homeLogo.png", 280, 288.4);
-        homeLogo.setLayoutX(getAppWidth()-((getAppWidth()/2.0)+140));
-        homeLogo.setLayoutY(20);
-
+        var logo = texture("homeLogo.png", 280, 288.4);
+        logo.setLayoutX((getAppWidth() / 2.0)-(logo.getWidth() / 2));
+        logo.setLayoutY(20);
         var body = createBody();
 
-        getContentRoot().getChildren().addAll(bg, homeLogo, body);
+        getContentRoot().getChildren().addAll(bg, logo, body);
     }
 
 
     @Override
     protected void onUpdate(double tpf) {
         super.onUpdate(tpf);
-        animations.forEach(animation -> {
-            animation.onUpdate(tpf);
-        });
+        animations.forEach(animation -> animation.onUpdate(tpf));
     }
 
     @Override
     public void onCreate() {
-        animIndex = 0;
-
-        buttons.forEach(btn -> {
-            //btn.setOpacity(1);
-        });
         animations.forEach(animation -> {
             animation.stop();
             animation.start();
@@ -66,49 +53,27 @@ public class MainMenu extends FXGLMenu {
     }
 
     private Node createBody() {
-        double midY = getAppHeight() / 2.0;
+        var platBtn = createButton("play_btn", this::fireNewGame);
+        var optionBtn = createButton("option_btn", this::fireNewGame);
+        var quitBtn = createButton("quit_btn", this::fireExit);
 
-        double distance = midY - 25;
-
-        var btnContinue = createActionButton(localizedStringProperty("menu.continue"), this::fireContinue);
-        var btn1 = createActionButton(localizedStringProperty("menu.newGame"), this::fireNewGame);
-        var btn2 = createActionButton(createStringBinding(() -> "PLACEHOLDER 1"), this::fireNewGame);
-        var btn3 = createActionButton(createStringBinding(() -> "PLACEHOLDER 2"), this::fireNewGame);
-        var btn4 = createActionButton(createStringBinding(() -> "PLACEHOLDER 3"), this::fireNewGame);
-        var btn5 = createActionButton(createStringBinding(() -> "PLACEHOLDER 4"), this::fireNewGame);
-        var btn6 = createActionButton(createStringBinding(() -> "PLACEHOLDER 5"), this::fireNewGame);
-        var btn7 = createActionButton(localizedStringProperty("menu.exit"), this::fireExit);
-
-        Group group = new Group(btnContinue, btn1, btn2, btn3, btn4, btn5, btn6, btn7);
-
-        double dtheta = Math.PI / (group.getChildren().size() - 1);
-        double angle = Math.PI / 2;
+        Group group = new Group(platBtn, optionBtn, quitBtn);
 
         int i = 0;
         for (Node n : group.getChildren()) {
-
-            Point2D vector = new Point2D(Math.cos(angle), -Math.sin(angle))
-                    .normalize()
-                    .multiply(distance)
-                    .add(0, midY);
-
-            n.setLayoutX(vector.getX() - (i == 0 || i == 7 ? 0 : 100));
-            n.setLayoutY(vector.getY());
-
-            angle -= dtheta;
-
-            // slightly hacky way to get a nice looking radial menu
-            // we assume that there are 8 items
-            if (i == 0 || i == group.getChildren().size() - 2) {
-                angle -= dtheta / 2;
-            } else if (i == 2 || i == 4) {
-                angle += dtheta / 4;
-            } else if (i == 3) {
-                angle += dtheta / 2;
-            }
-
+            n.setLayoutY((n.getBoundsInLocal().getHeight() * (i*1.2)));
+            n.scaleXProperty().bind(
+                    Bindings.when(n.hoverProperty()).then(1.1).otherwise(1)
+            );
+            n.scaleYProperty().bind(
+                    Bindings.when(n.hoverProperty()).then(1.1).otherwise(1)
+            );
             i++;
         }
+
+
+        group.setLayoutY((getAppHeight() / 2.0)+(group.getBoundsInLocal().getHeight() / 3));
+        group.setLayoutX((getAppWidth() / 2.0)-(group.getBoundsInLocal().getWidth() / 2));
 
         return group;
     }
@@ -116,29 +81,17 @@ public class MainMenu extends FXGLMenu {
     /**
      * Creates a new button with given name that performs given action on click/press.
      *
-     * @param name  button name (with binding)
      * @param action button action
      * @return new button
      */
-    private Node createActionButton(StringBinding name, Runnable action) {
-        var bg = new Rectangle(200, 50);
-        bg.setEffect(new BoxBlur());
+    private Node createButton(String file, Runnable action) {
+        var bg = texture(file+".png", 674.54/2, 83/2);
 
-        var text = getUIFactoryService().newText(name);
-        text.setTranslateX(15);
-        text.setFill(Color.BLACK);
-
-        var btn = new StackPane(bg, text);
-
-        bg.fillProperty().bind(when(btn.hoverProperty())
-                .then(Color.LIGHTGREEN)
-                .otherwise(Color.DARKGRAY)
-        );
+        var btn = new StackPane(bg);
 
         btn.setAlignment(Pos.CENTER_LEFT);
         btn.setOnMouseClicked(e -> action.run());
 
-        // clipping
         buttons.add(btn);
         animations.add(animationBuilder(this)
                 .delay(Duration.seconds(animIndex * 0.1))
@@ -152,11 +105,9 @@ public class MainMenu extends FXGLMenu {
                 .fadeIn(btn)
                 .build());
 
-        Rectangle clip = new Rectangle(200, 50);
-        clip.translateXProperty().bind(btn.translateXProperty().negate());
+        animIndex++;
 
         btn.setTranslateX(-200);
-        btn.setClip(clip);
         btn.setCache(true);
         btn.setCacheHint(CacheHint.SPEED);
 
