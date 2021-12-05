428d0d0847ce59fb408038f7fa9bc340844e980b
tigerza117 <noppawanza1171@gmail.com>
2021-12-05 22:27:53 +0700

    ✨: Basic game menu.

--- src/main/java/co/yunchao/client/views/Home.java
+++ src/main/java/co/yunchao/client/views/Home.java
@@ -1,34 +1,52 @@
 package co.yunchao.client.views;
 
+import com.almasb.fxgl.achievement.Achievement;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.MenuItem;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 
+import java.util.Arrays;
+import java.util.EnumSet;
+
 public class Home extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setTitle("Legendary BlackJack");
-        settings.setHeight(720);
-        settings.setWidth(1280);
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setMainMenuEnabled(true);
+        settings.setGameMenuEnabled(true);
+        settings.setFullScreenAllowed(true);
+        settings.setEnabledMenuItems(EnumSet.of(MenuItem.EXTRA));
+        settings.getCredits().addAll(Arrays.asList(
+                "Short Name - Lead Programmer",
+                "LongLongLongLongLongLongLong Name - Programmer",
+                "V Short - Artist",
+                "Medium-Hyphen Name - Designer",
+                "More Credits - 111",
+                "More Credits - 222",
+                "More Credits - 333",
+                "More Credits - 444",
+                "More Credits - 444",
+                "Example of a credit name that will definitely not fit on the screen using default font",
+                "More Credits - 444",
+                "More Credits - 444",
+                "More Credits - 555",
+                "More Credits - 666",
+                "More Credits - 777"
+        ));
+
+        settings.getAchievements().add(new Achievement("Name", "description", "", 0));
+        settings.getAchievements().add(new Achievement("Name2", "description2", "", 1));
     }
-    protected void initMainMenu(Pane mainMenuRoot){
-        Rectangle homeBg = new Rectangle(1280, 720);
 
-        Button playBtn = new Button("Play");
-        Button optBtn = new Button("Options");
-        Button qBtn = new Button("Quit");
-
-        VBox btnBox = new VBox(50, playBtn,optBtn,qBtn);
-        btnBox.setTranslateX(400);
-        btnBox.setTranslateY(200);
-
-        mainMenuRoot.getChildren().addAll(homeBg, btnBox);
+    @Override
+    protected void initGame() {
     }
-
 
     public static void main(String[] args) {
         launch(args);
