339cb8a9acab4a5737451564cb81821e88fb0f7d
tigerza117 <noppawanza1171@gmail.com>
2021-12-12 14:04:15 +0700

    ✨ Client player controller.

--- src/main/java/co/yunchao/client/controllers/PlayerController.java
+++ src/main/java/co/yunchao/client/controllers/PlayerController.java
@@ -1,4 +1,19 @@
 package co.yunchao.client.controllers;
 
+import co.yunchao.client.views.Seat;
+
 public class PlayerController {
+    private final Seat seat;
+
+    public PlayerController(Seat seat) {
+        this.seat = seat;
+    }
+
+    private void sit() {
+        seat.sit();
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
 }
