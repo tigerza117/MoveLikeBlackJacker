4d46e7e29f15379f2d68bd5df61ce8f4a0267b9d
tigerza117 <noppawanza1171@gmail.com>
2021-12-12 14:04:30 +0700

    ✨ Client game controller.

--- src/main/java/co/yunchao/client/controllers/GameController.java
+++ src/main/java/co/yunchao/client/controllers/GameController.java
@@ -1,25 +1,40 @@
 package co.yunchao.client.controllers;
 
-import co.yunchao.client.views.Card;
-import co.yunchao.client.views.Chip;
+import co.yunchao.client.net.Network;
 import co.yunchao.client.views.Table;
+import co.yunchao.net.packets.ConnectPacket;
+import co.yunchao.net.packets.PlayerJoinPacket;
+
+import java.util.ArrayList;
 
 public class GameController {
     private final Table view;
+    private final ArrayList<PlayerController> playerControllers;
+    private final Network network;
 
     public GameController() {
-        view = new Table();
+        this.network = new Network(this);
+        this.view = new Table();
+        this.playerControllers = new ArrayList<>();
     }
 
     public void Start() {
+        network.putPacket(new ConnectPacket());
         view.render();
         view.getSeats().forEach((s, seat) -> {
             seat.sit();
-            seat.addCard(new Card("05"));
-            seat.addCard(new Card("55"));
-            seat.addChipBet(new Chip("chip1"));
-            seat.addChipBet(new Chip("chip2"));
-            seat.addChipBet(new Chip("chip3"));
         });
+    }
+
+    public void handlerPlayerJoin(PlayerJoinPacket packet) {
+        System.out.println("Player " + packet.getId() + " has been join");
+    }
+
+    public void handlerPlayerLevel(PlayerJoinPacket packet) {
+
+    }
+
+    public void handlerPlayerAction(PlayerJoinPacket packet) {
+
     }
 }
