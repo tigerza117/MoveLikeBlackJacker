21b068bb87341ef547a77c4092ea8632cf9fe383
santhitak <santhita.sk@gmail.com>
2021-12-12 23:24:51 +0700

    :bug::lipstick: fix: styling and fix enter field not show

--- src/main/java/co/yunchao/client/views/PlayModal.java
+++ src/main/java/co/yunchao/client/views/PlayModal.java
@@ -1,16 +1,16 @@
 package co.yunchao.client.views;
 
 import javafx.scene.Group;
-import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.GaussianBlur;
-
-import java.util.HashMap;
 
 import static com.almasb.fxgl.dsl.FXGL.texture;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
 
 public class PlayModal {
+
     private final Group group;
+    TextField codeField;
 
     public PlayModal(){
         group = new Group();
@@ -20,22 +20,38 @@ public class PlayModal {
         var banner = texture("/enterRoom/yellow_banner.png", getAppWidth(), 500);
         banner.setLayoutY((getAppHeight() / 2.0)-(banner.getHeight() / 2));
 
-        var createBtn = Button.create("/enterRoom/createRoom", () -> {
-            banner.setVisible(false);
-            playMenu.setVisible(false);
-            enterNameAc.getGroup().setVisible(true);
-        });
-        createBtn.setTranslateY(-300);
-
         var orSep = texture("/enterRoom/Or.png");
         orSep.setLayoutY(-180);
 
-        var enterCodeBtn = Button.create("/enterRoom/enterNum", () -> {
+        var enterCodeBtn = texture("/enterName/textField.png");
+        enterCodeBtn.setTranslateY(-50);
+
+        codeField = new TextField("Enter Room");
+        codeField.setMaxWidth(380);
+        codeField.getStylesheets().add("/css/style.css");
+        codeField.getStyleClass().add("text-field");
+        codeField.setLayoutY((getAppHeight() / 2.0) - ((codeField.getHeight() / 2)-92));
+
+        var confirmBtn = Button.create("/enterName/confirmBtn", () -> {
             banner.setVisible(false);
             playMenu.setVisible(false);
+            codeField.setVisible(false);
             enterNameAc.getGroup().setVisible(true);
         });
-        enterCodeBtn.setTranslateY(-50);
+
+        confirmBtn.setLayoutY((getAppHeight() / 2.0) - ((confirmBtn.getBoundsInLocal().getHeight() / 2)-130));
+        confirmBtn.setLayoutX((getAppWidth()/2.0) - ((confirmBtn.getBoundsInLocal().getWidth()/2.0)-200));
+
+        codeField.setLayoutX((getAppWidth()/2.0) - ((confirmBtn.getBoundsInLocal().getWidth()/2.0)+200));
+
+        var createBtn = Button.create("/enterRoom/createRoom", () -> {
+            banner.setVisible(false);
+            playMenu.setVisible(false);
+            codeField.setVisible(false);
+            confirmBtn.setVisible(false);
+            enterNameAc.getGroup().setVisible(true);
+        });
+        createBtn.setTranslateY(-300);
 
         playMenu.getChildren().addAll(createBtn, orSep, enterCodeBtn);
 
@@ -43,7 +59,8 @@ public class PlayModal {
         playMenu.setLayoutY((getAppHeight() / 2.0)+(playMenu.getBoundsInLocal().getHeight() / 3));
         playMenu.setLayoutX((getAppWidth() / 2.0)-(playMenu.getBoundsInLocal().getWidth() / 2));
 
-        group.getChildren().addAll(banner, playMenu, enterNameAc.getGroup());
+        group.getChildren().addAll(banner, playMenu, confirmBtn, codeField, enterNameAc.getGroup());
+
         group.setVisible(false);
     }
 
