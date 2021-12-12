01d79def28042444a06731f944b55f78131574aa
santhitak <santhita.sk@gmail.com>
2021-12-12 23:25:02 +0700

    :bug::lipstick: fix: styling and fix enter field not show

--- src/main/java/co/yunchao/client/views/enterNameAction.java
+++ src/main/java/co/yunchao/client/views/enterNameAction.java
@@ -3,6 +3,7 @@ package co.yunchao.client.views;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 
 import java.util.HashMap;
 
@@ -13,6 +14,7 @@ public class enterNameAction {
 
     private static Group group;
     private final HashMap<String, Node> buttons;
+    TextField nameField;
 
     public enterNameAction(){
         group = new Group();
@@ -20,19 +22,28 @@ public class enterNameAction {
         var namePane = texture("/enterName/namePanel.png", getAppWidth(), 306);
         namePane.setLayoutY((getAppHeight() / 2.0)-(namePane.getHeight() / 2));
 
-        var nameField = texture("/enterName/textField.png");
-        nameField.setLayoutY((getAppHeight() / 2.0)-((nameField.getHeight() / 2)-50));
-        nameField.setLayoutX((getAppWidth()/2.0) - (nameField.getWidth()/2.0));
+        var textField = texture("/enterName/textField.png");
+        textField.setLayoutY((getAppHeight() / 2.0)-((textField.getHeight() / 2)-50));
+        textField.setLayoutX((getAppWidth()/2.0) - (textField.getWidth()/2.0));
 
         var confirmBtn = Button.create("/enterName/confirmBtn", () -> FXGL.getGameController().startNewGame());
         confirmBtn.setLayoutY((getAppHeight() / 2.0) - ((confirmBtn.getBoundsInLocal().getHeight() / 2)-50));
         confirmBtn.setLayoutX((getAppWidth()/2.0) - ((confirmBtn.getBoundsInLocal().getWidth() /2.0)-200));
 
+        nameField = new TextField("Your Name");
+        nameField.setMaxWidth(380);
+        nameField.getStylesheets().add("/css/style.css");
+        nameField.getStyleClass().add("text-field");
+        nameField.setLayoutY((getAppHeight() / 2.0) - ((nameField.getHeight() / 2)-14));
+        nameField.setLayoutX((getAppWidth()/2.0) - ((confirmBtn.getBoundsInLocal().getWidth() /2.0)+200));
+
+        Group enterPane = new Group(textField, nameField, confirmBtn);
+
         buttons = new HashMap<>() {{
             put("confirm", confirmBtn);
         }};
 
-        group.getChildren().addAll(namePane, nameField, confirmBtn);
+        group.getChildren().addAll(namePane, enterPane);
         group.setVisible(false);
     }
 
