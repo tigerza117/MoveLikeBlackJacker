eca5f63b1b4d287184ded161f7833443fec3aab6
santhitak <santhita.sk@gmail.com>
2021-12-14 03:38:40 +0700

    :zap: fix: turn modal into SubScene

--- src/main/java/co/yunchao/client/views/LeaveModal.java
+++ src/main/java/co/yunchao/client/views/LeaveModal.java
@@ -1,40 +1,41 @@
 package co.yunchao.client.views;
 
+import com.almasb.fxgl.scene.Scene;
 import javafx.scene.Group;
+import com.almasb.fxgl.scene.SubScene;
+import javafx.scene.effect.GaussianBlur;
+import org.jetbrains.annotations.NotNull;
 
-import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
+import static com.almasb.fxgl.dsl.FXGL.*;
 
-public class LeaveModal {
-    private final Group group;
+public class LeaveModal extends SubScene {
 
     public LeaveModal(){
-        group = new Group();
 
         var quitPane = texture("leaveGame/leavePane.png", getAppWidth(), 290);
         quitPane.setLayoutY((getAppHeight() / 2.0)-(quitPane.getHeight() / 2));
 
         var yesBtn = Button.create("leaveGame/yesBtn", () -> getGameController().exit());
-        var noBtn = Button.create("leaveGame/noBtn", this::close);
+        var noBtn = Button.create("leaveGame/noBtn", () -> getSceneService().popSubScene());
         noBtn.setLayoutX(200);
 
         Group leaveMenu = new Group(yesBtn, noBtn);
 
         leaveMenu.setLayoutY((getAppHeight() / 2.0)+(leaveMenu.getBoundsInLocal().getHeight() / 3.5));
         leaveMenu.setLayoutX((getAppWidth() / 2.0)-(leaveMenu.getBoundsInLocal().getWidth() / 2));
-        
-        group.getChildren().addAll(quitPane, leaveMenu);
-        group.setVisible(false);
+
+        getContentRoot().getChildren().addAll(quitPane, leaveMenu);
     }
 
-    public void render() {
-        group.setVisible(true);
+    @Override
+    public void onEnteredFrom(@NotNull Scene prevState) {
+        super.onEnteredFrom(prevState);
+        prevState.getContentRoot().setEffect(new GaussianBlur());
     }
 
-    public void close() {
-        group.setVisible(false);
-    }
-
-    public Group getGroup() {
-        return group;
+    @Override
+    public void onExitingTo(@NotNull Scene nextState) {
+        super.onExitingTo(nextState);
+        nextState.getContentRoot().setEffect(null);
     }
 }
